5fcf7a5803ca6cf1c0147a6b8ab0096aa09f78ca
Klemen <klemenpavlovic@gmail.com>
2020-12-28 18:23:49 +0100

    Fix navmenu on resize in browser

--- scripts.js
+++ scripts.js
@@ -8,4 +8,19 @@ function mobileMenu() {
       x.style.display = "block";
       y.style.marginTop = "18rem";
     }
-  } 
+  }
+
+function onResize() {
+  var x = document.getElementById("navbar");
+  var y = document.getElementById("main-doc")
+  var w = window.innerWidth;
+  if (w > 768) {
+    x.style.display = "block";
+    y.style.marginTop ="5rem";
+  } else {
+    x.style.display = "none";
+    y.style.marginTop = "2rem";
+  }
+}
+
+window.addEventListener("resize", onResize);
